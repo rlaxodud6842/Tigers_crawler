5be8aa96e2193bcad7c9c734cd7f0131a9182bca
daeekim <asdxl1030@naver.com>
2024-05-22 19:38:38 +0900

    DB

---
+++ web/models/index.js
@@ -0,0 +1,19 @@
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('mydatabase', 'myuser', 'mypassword', {
+  host: 'localhost',
+  dialect: 'mysql'
+});
+
+const User = require('./user')(sequelize);
+
+sequelize.sync().then(() => {
+  console.log('Database & tables created!');
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
+});
+
+module.exports = {
+  sequelize,
+  User
+};
---
+++ web/models/user.js
@@ -0,0 +1,17 @@
+const { DataTypes } = require('sequelize');
+
+module.exports = (sequelize) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+
+  return User;
+};
