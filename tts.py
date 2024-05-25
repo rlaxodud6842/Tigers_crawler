70c12c63d252ae43fe429f94dc0b53eab7ef09a3
rlaxodud6842 <rlaxodud6842@naver.com>
2024-05-25 14:37:53 +0900

    tts 구현

---
+++ tts.py
@@ -0,0 +1,16 @@
+import gtts
+import playsound as ps
+import os
+class TTS():
+    def play_sound(self,sentence):
+        print("TTS 변환중")
+        """
+                English(United States) en
+                French(France) fr
+                Korean(Korea) ko
+                Spanish(Spain) es
+                Spanish(UnitedStates) es
+        """
+        tts = gtts.gTTS(text=sentence, lang = 'ko')
+        tts.save("grade_voice.mp3")
+        ps.playsound("grade_voice.mp3")
