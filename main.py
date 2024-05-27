b9484b1227e5aac02d7cdaf7cc4edc9d8ad5189c
daeekim <asdxl1030@naver.com>
2024-05-27 12:01:51 +0900

    update main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import Tools.chatgpt
 from tts import TTS
+import argparse
 
-def main():
+def main(username, password, year, semester):
+    print(f"Year: {year}, Semester: {semester}")  # 값 확인
     tts = TTS()
-    sentence = Tools.chatgpt.crawler_and_gpt()
+    sentence = Tools.chatgpt.crawler_and_gpt(username, password, year, semester)
     tts.play_sound(sentence)
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="Process some integers.")
+    parser.add_argument('username', type=str, help='아이디')
+    parser.add_argument('password', type=str, help='비밀번호')
+    parser.add_argument('year', type=int, help='년도')
+    parser.add_argument('semester', type=int, help='학기')
+    args = parser.parse_args()
+    
+    main(args.username, args.password, args.year, args.semester)
+
